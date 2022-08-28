6ab5eb61e8959485b9f2d8d701c6ed1687491f55
Tao Bian <biantao0519@hotmail.com>
2022-08-28 17:58:42 +1000

    fixed some issue and tested all the routes

--- controller/api/petRoute.js
+++ controller/api/petRoute.js
@@ -8,9 +8,6 @@ const withAuth = require('../../utils/auth')
 router.get('/', (req, res) => {
     Pet.findAll({
         // Get all pets info include user info
-        order: [
-            ['created_at', 'DESC']
-        ],
         include: [
             {
                 model: User,
@@ -28,44 +25,46 @@ router.get('/', (req, res) => {
 })
 
 
-//GET one pet by id ---------------------- 
-router.get('/:id', withAuth, (req, res) => {
-    Pet.findOne({
-        where: { id: req.params.id },
-        include: [
-            {
-                model: User,
-                attributes: ['username', 'email', 'phoneNumber']
-            },
-        ]
-    })
-        .then(petData => {
-            // if not found, return message
-            if (!petData) {
-                return res.status(404).json({ message: 'Cannot found pet by this id!' })
-            }
-            // else, return data
-            res.status(200).json(petData)
-        })
-        // handle err
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-})
+// //GET one pet by id ---------------------- 
+// router.get('/:id',
+//     // withAuth, // TODO: uncomment this after finish testing 
+//     (req, res) => {
+//         Pet.findOne({
+//             where: { id: req.params.id },
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['username', 'email', 'phoneNumber']
+//                 },
+//             ]
+//         })
+//             .then(petData => {
+//                 // if not found, return message
+//                 if (!petData) {
+//                     return res.status(404).json({ message: 'Cannot found pet by this id!' })
+//                 }
+//                 // else, return data
+//                 res.status(200).json(petData)
+//             })
+//             // handle err
+//             .catch(err => {
+//                 console.log(err)
+//                 res.status(500).json(err)
+//             })
+//     })
 
 
 
 // GET pets by user search input ------------- not completed yet
-router.get('/search', 
+router.get('/search',
     // withAuth,  // TODO: uncomment this after finish testing
     (req, res) => {
         Pet.findAll({
-            where: { 
+            where: {
                 typeOfPet: req.body.typeOfPet,
                 breed: req.body.breed,
                 gender: req.body.gender,
-                location: req.body.location 
+                location: req.body.location
             },
             include: [
                 {
@@ -74,20 +73,20 @@ router.get('/search',
                 },
             ]
         })
-        .then(petData => {
-            // if not found, return message
-            if (!petData) {
-                return res.status(404).json({ message: 'Cannot found any pets!' })
-            }
-            // else, return data
-            res.status(200).json(petData)
-        })
-        // handle err
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-})
+            .then(petData => {
+                // if not found, return message
+                if (!petData) {
+                    return res.status(404).json({ message: 'Cannot found any pets!' })
+                }
+                // else, return data
+                res.status(200).json(petData)
+            })
+            // handle err
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            })
+    })
 
 
 
@@ -95,32 +94,11 @@ router.get('/search',
 
 
 // Create a new pet
-router.post('/', withAuth, (req, res) => {
-    Pet.create({
-        typeOfPet: req.body.typeOfPet,
-        name: req.body.name,
-        breed: req.body.breed,
-        age: req.body.age,
-        gender: req.body.gender,
-        description: req.body.description,
-        location: req.body.location,
-        user_id: req.session.user_id
-    })
-        // if no err, return data
-        .then(petData => res.status(200).json(petData))
-        // handle err
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-})
-
-
-// Update a pet
-router.put('/:id', withAuth, (req, res) => {
-    Pet.update(
-        {
-            type: req.body.type,
+router.post('/',
+    // withAuth, // TODO: uncomment this after finish testing 
+    (req, res) => {
+        Pet.create({
+            typeOfPet: req.body.typeOfPet,
             name: req.body.name,
             breed: req.body.breed,
             age: req.body.age,
@@ -128,40 +106,67 @@ router.put('/:id', withAuth, (req, res) => {
             description: req.body.description,
             location: req.body.location,
             user_id: req.session.user_id
-        },
-        {
-            where: { id: req.params.id }
         })
-        .then(petData => {
-            if (!petData[0]) {
-                return res.status(404).json({ message: 'Cannot found pet by this id!' })
-            }
-            res.status(200).json(petData)
-        })
-        // handle err
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-})
+            // if no err, return data
+            .then(petData => res.status(200).json(petData))
+            // handle err
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            })
+    })
+
+
+// Update a pet
+router.put('/:id',
+    // withAuth, // TODO: uncomment this after finish testing 
+    (req, res) => {
+        Pet.update(
+            {
+                type: req.body.type,
+                name: req.body.name,
+                breed: req.body.breed,
+                age: req.body.age,
+                gender: req.body.gender,
+                description: req.body.description,
+                location: req.body.location,
+                user_id: req.session.user_id
+            },
+            {
+                where: { id: req.params.id }
+            })
+            .then(petData => {
+                if (!petData[0]) {
+                    return res.status(404).json({ message: 'Cannot found pet by this id!' })
+                }
+                res.status(200).json(petData)
+            })
+            // handle err
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            })
+    })
 
 
 // DELETE a pet
-router.delete('/:id', withAuth, (req, res) => {
-    Pet.destroy({
-        where: { id: req.params.id }
+router.delete('/:id',
+    // withAuth, // TODO: uncomment this after finish testing
+    (req, res) => {
+        Pet.destroy({
+            where: { id: req.params.id }
+        })
+            .then(petData => {
+                if (!petData) {
+                    return res.status(404).json({ message: 'Cannot found pet by this id!' })
+                }
+                res.status(200).json(petData)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            })
     })
-        .then(petData => {
-            if (!petData) {
-                return res.status(404).json({ message: 'Cannot found pet by this id!' })
-            }
-            res.status(200).json(petData)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-})
 
 
 module.exports = router
